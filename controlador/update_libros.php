9a3655d4302099cd4267455f7dc97c96b2bf3b84
Samuel-FF <42384969+Samuel-FF@users.noreply.github.com>
2019-10-11 17:54:18 -0500

    Biblioteca/Proyecto
    update de controlador bello :v

--- controlador/update_libros.php
+++ controlador/update_libros.php
@@ -1,29 +1,29 @@
 <?php
 //condicion donde se rectifica que los campos no esten vacios y que esten definidos
-if(isset($_POST['actualizar']) && !empty($_POST['seleccion_libro'])){
+if(isset($_POST['actualizar_libro']) && !empty($_POST['titulo']) && !empty($_POST['editorial']) && !empty($_POST['autor']) && !empty($_POST['fecha'])){
 
         require_once '../modelo/MySQL.php';//se llama la pagina mysql.php para hacer la respectiva conexion con la BD
         //declaracion de las variables donde se almacenan los datos de los respectivos campos llenados del formulario
-        $seleccion=$_POST["seleccion_libro"];
-
+        $titulo = $_POST["titulo"];
+        $editorial = $_POST["editorial"];
+        $autor = $_POST["autor"];
+        $fecha = $_POST["fecha"];
+        $id = $_POST['id'];
         
 
         $mysql = new MySQL;//nuevo mysql
         $mysql->conectar();//funcion almacendad en mysql.php
         //consulta de la insercion de datos en la base de datos, donde hace las respectivas consultas
-        $sql=$mysql->efectuarConsulta("update biblioteca3.libros set (titulo_libro = seleccion_libro) where id_libro = 2");
-
-       // $sql=$mysql->efectuarconsulta("insert into biblioteca3.estudiantes values ('','ndocuemnto','$nombre','$apellido','pass','documento','estado','programa')");
-
-        //$cs=mysql_query($sql,$cn);
+        $sql=$mysql->efectuarConsulta("UPDATE biblioteca3.libros SET titulo_libro ='".$titulo."',editorial = '".$editorial."',autor = '".$autor."',fecha_publicacion = '".$fecha."' WHERE id_libro = ".$id."");
+        
+echo "UPDATE biblioteca3.libros SET titulo_libro ='".$titulo."',editorial = '".$editorial."',autor = '".$autor."',fecha_publicacion = '".$fecha."' WHERE id_libro = ".$id."";
         //condiciones de redirecionamiento a las respectivas paginas
         if($sql){
-            echo "<script>alert('Se registro correctamente');</script>";
-            header("location: ../inventario_Bibliotecario.php");
+
+            echo"<script type=\"text/javascript\">alert('Se actualizo correctamente!'); window.location='../inventario_Bibliotecario.php';</script>";
         }else{
-            echo "<script>alert('Se produjo un error');</script>";
-            //header("location: ../añadir_libro.php");
-        }
+            echo"<script type=\"text/javascript\">alert('Se produjo un error!'); window.location='../actualizar_libro.php';</script>";
+        } 
         
          
 }
