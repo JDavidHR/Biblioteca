d4c1a7aaf9c4aaa1d0e3a96c6fabdf95a47e45eb
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2019-10-28 16:04:08 -0500

    Biblioteca/Proyecto

--- controlador/register_bibliotecario_controlador.php
+++ controlador/register_bibliotecario_controlador.php
@@ -14,21 +14,29 @@ if(isset($_POST['registrarb']) && !empty($_POST['nombreb']) && !empty($_POST['ap
     $mysql = new MySQL;//nuevo mysql
     $mysql->conectar();//funcion llamada desde mysql.php
 
+
+    $consultadoc =$mysql->efectuarConsulta("SELECT numero_documento FROM biblioteca3.estudiantes WHERE numero_documento = ".$ndocumentob."");
+
+
     //consulta almacenada en una variable junto a la funcion de de mysql.php
     $consulta =$mysql->efectuarConsulta("SELECT numero_documento FROM biblioteca3.bibliotecario WHERE numero_documento = ".$ndocumentob."");
 
-    //condicion para ver si el documento a registrar existe, si el valor es mayor a 1 es porque encontro un resultado en la BD, y si es cero es porque no encontro nada
-    if(mysqli_num_rows($consulta)>0){
-        //mensaje de salida (alert) en caso de que el documento ya exista junto con su redireccion
-         echo"<script type=\"text/javascript\">alert('El documento ya se encuentra registrado'); window.location='../register_bibliotecario.php';</script>";
-    }else{
-            //declaracion de variable que almacena la consulta de insercion a la BD junto a la respectiva funcion de mysql.php y su redireccion de pagina
-            $sql=$mysql->efectuarConsulta("insert into biblioteca3.bibliotecario(nombre,apellido,numero_documento,contrasena,estado_civil_id_estado,tipo_documento_id_tipo,estado) VALUES ('".$nombreb."','".$apellidob."',".$ndocumentob.",'".$passb."','".$estadob."','".$documentob."',1)");
-            //mensaje de salida (alert) en caso de que el documento no exista junto con su redireccion
-            echo"<script type=\"text/javascript\">alert('Se registro correctamente!'); window.location='../gestion_Bibliotecarios.php';</script>";
 
+ if(mysqli_num_rows($consultadoc)){
+         echo"<script type=\"text/javascript\">alert('El documento a registrar ya se encuentra registrado como un estudiante'); window.location='../register_bibliotecario.php';</script>";
+    }else{
+        //condicion para ver si el documento a registrar existe, si el valor es mayor a 1 es porque encontro un resultado en la BD, y si es cero es porque no encontro nada
+        if(mysqli_num_rows($consulta)>0){
+            //mensaje de salida (alert) en caso de que el documento ya exista junto con su redireccion
+             echo"<script type=\"text/javascript\">alert('El documento ya se encuentra registrado'); window.location='../register_bibliotecario.php';</script>";
+        }else{
+                //declaracion de variable que almacena la consulta de insercion a la BD junto a la respectiva funcion de mysql.php y su redireccion de pagina
+                $sql=$mysql->efectuarConsulta("insert into biblioteca3.bibliotecario(nombre,apellido,numero_documento,contrasena,estado_civil_id_estado,tipo_documento_id_tipo,estado) VALUES ('".$nombreb."','".$apellidob."',".$ndocumentob.",'".$passb."','".$estadob."','".$documentob."',1)");
+                //mensaje de salida (alert) en caso de que el documento no exista junto con su redireccion
+                echo"<script type=\"text/javascript\">alert('Se registro correctamente!'); window.location='../gestion_Bibliotecarios.php';</script>";
+
+        }
     }
 }
-
 
 ?>
