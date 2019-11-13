d66d2e8e44e266df8d6f835918b6b51b69ae7fe5
JDavidHR <56202293+JDavidHR@users.noreply.github.com>
2019-11-13 08:46:44 -0500

    Add files via upload

---
+++ data.php
@@ -0,0 +1,132 @@
+<table id="example" class="table table-striped table-bordered" style="width:100%"> 
+	<thead> 
+		<tr> 
+			<th>Name</th> 
+			<th>Position</th> 
+			<th>Office</th> 
+			<th>Age</th> 
+			<th>Start date</th>
+			<th>Salary</th> 
+		</tr> 
+	</thead> 
+	<tbody> 
+		<tr> 
+			<td>Tiger Nixon</td> 
+			<td>System Architect</td> 
+			<td>Edinburgh</td>
+			<td>61</td> 
+			<td>2011/04/25</td> 
+			<td>$320,800</td> 
+		</tr> 
+		<tr> 
+			<td>Garrett Winters</td> 
+			<td>Accountant</td> 
+			<td>Tokyo</td> 
+			<td>63</td> 
+			<td>2011/07/25</td> 
+			<td>$170,750</td> 
+		</tr> 
+		<tr> 
+			<td>Ashton Cox</td> 
+			<td>Junior Technical Author</td> 
+			<td>San Francisco</td> 
+			<td>66</td> 
+			<td>2009/01/12</td> 
+			<td>$86,000</td> 
+		</tr> 
+		<tr> 
+			<td>Cedric Kelly</td> 
+			<td>Senior Javascript Developer</td> 
+			<td>Edinburgh</td> 
+			<td>22</td> 
+			<td>2012/03/29</td> 
+			<td>$433,060</td> 
+		</tr> 
+		<tr> 
+			<td>Airi Satou</td> 
+			<td>Accountant</td> 
+			<td>Tokyo</td> 
+			<td>33</td> 
+			<td>2008/11/28</td> 
+			<td>$162,700</td> 
+		</tr> 
+		<tr> 
+			<td>Brielle Williamson</td> 
+			<td>Integration Specialist</td> 
+			<td>New York</td> 
+			<td>61</td> 
+			<td>2012/12/02</td> 
+			<td>$372,000</td> 
+		</tr> 
+		<tr> 
+			<td>Herrod Chandler</td> 
+			<td>Sales Assistant</td> 
+			<td>San Francisco</td> 
+			<td>59</td>
+			<td>2012/08/06</td> 
+			<td>$137,500</td> 
+		</tr> 
+		<tr> 
+			<td>Rhona Davidson</td> 
+			<td>Integration Specialist</td> 
+			<td>Tokyo</td> 
+			<td>55</td> 
+			<td>2010/10/14</td> 
+			<td>$327,900</td> 
+		</tr> 
+		
+		</tbody> 
+		<tfoot> 
+			<tr> 
+				<th>Name</th> 
+				<th>Position</th> 
+				<th>Office</th> 
+				<th>Age</th> 
+				<th>Start date</th> 
+				<th>Salary</th> 
+			</tr> 
+		</tfoot> 
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0">
+		<tbody>
+			<tr>
+				<td class="gutter">
+					<div class="line number1 index0 alt2">1</div>
+					<div class="line number2 index1 alt1">2</div>
+					<div class="line number3 index2 alt2">3</div>
+				</td>
+				<td class="code">
+					<div class="container">
+						<div class="line number1 index0 alt2">
+							<code class="js plain">$(document).ready(</code>
+							<code class="js keyword">function</code>
+							<code class="js plain">() {</code>
+						</div>
+						<div class="line number2 index1 alt1">
+							<code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code>
+							<code class="js plain">$(</code>
+							<code class="js string">'#example'</code>
+							<code class="js plain">).DataTable();</code>
+						</div>
+						<div class="line number3 index2 alt2">
+							<code class="js plain">} );</code>
+						</div>
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0">
+		<tbody>
+			<tr>
+				<td class="gutter">
+					<div class="line number1 index0 alt2" style="display: none;">1</div>
+				</td>
+				<td class="code">
+					<div class="container" style="display: none;">
+						<div class="line number1 index0 alt2" style="display: none;">&nbsp;</div>
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
